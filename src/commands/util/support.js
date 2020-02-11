1ae7fc75dcb889ff25ea6d46c2ee4de75de4c5b3
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2020-02-11 11:06:17 -0800

    Update support.js

--- src/commands/util/support.js
+++ src/commands/util/support.js
@@ -17,10 +17,14 @@ module.exports = class SCommand extends Command {
         })
     }
     async run(message) {
+        try{
         let embed = new Discord.MessageEmbed()
             .setAuthor(`Elara Support`, "https://cdn.discordapp.com/icons/499409162661396481/28c6fa39e722e2c0aea60f15ca105c1d.png?size=2048")
             .setColor(message.guild ? message.guild.color : this.client.util.colors.default)
             .setDescription(this.client.options.invite)
-        message.say(embed)
+        message.say(embed).catch(() => {});
+        }catch(e){
+          this.client.handleError(this.client, message, e)
+        }
     }
 }
