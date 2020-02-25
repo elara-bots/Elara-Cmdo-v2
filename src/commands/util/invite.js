1a4b613ca7aee6a3223d720a7be84d187644ea1c
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2020-02-25 10:27:05 -0800

    Update invite.js

--- src/commands/util/invite.js
+++ src/commands/util/invite.js
@@ -4,19 +4,25 @@ module.exports = class NCommand extends Command {
         super(client, {
             name: "invite",
             memberName: "invite",
-            aliases: ["botinvite", `inv`],
+            aliases: ["botinvite", `inv`, `bot`],
             examples: [`${client.commandPrefix}invite`],
             description: "Gives you a invite for the bot.",
             clientPermissions: ["EMBED_LINKS", "SEND_MESSAGES"],
-            throttling: {
-                usages: Globalcooldown.usage,
-                duration: Globalcooldown.duration
-            },
-            group: "bot"
+            throttling: Globalcooldown.default,
+            group: "bot",
+            args: [
+                {
+                    key: "user",
+                    prompt: "What bot?",
+                    type: "user",
+                    default: msg => msg.client.user
+                }
+            ]
         })
     }
-    async run(message) {
+    async run(message, {user}) {
         try{
+        if(!user.bot) return this.client.error(message, `${this.client.getEmoji("nemoji")} You provided a user account... NOT a bot!`)
         let links = [
             `[All Permissions](https://discordapp.com/oauth2/authorize?client_id=${this.client.user.id}&permissions=2137517567&scope=bot)`,
             `[Administrator Permissions](https://discordapp.com/api/oauth2/authorize?client_id=${this.client.user.id}&permissions=8&scope=bot)`,
@@ -27,7 +33,10 @@ module.exports = class NCommand extends Command {
         let e = new Discord.MessageEmbed()
         .setColor(message.guild ? message.guild.color : this.client.util.colors.default)
         .setAuthor(`Bot Invites`, this.client.user.displayAvatarURL())
-        .setDescription(links.join('\n') + `\n[Support Server](${this.client.options.invite})`)
+        .setDescription(links.join('\n'))
+        if(user.id === this.client.user.id){
+            e.normalizeField(`Support`, this.client.options.invite);
+        }
         return message.channel.send(e)
         }catch(e){
            this.client.handleError(this.client, message, e)
