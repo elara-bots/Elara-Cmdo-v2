e386d7ac34f4b56f04f8679c8047455f0a7b239e
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2020-03-08 17:21:40 -0700

    Update support.js

--- src/commands/util/support.js
+++ src/commands/util/support.js
@@ -10,17 +10,17 @@ module.exports = class SCommand extends Command {
             description: "Gives you the invite to the support server",
             group: "bot",
             clientPermissions: ["EMBED_LINKS", "SEND_MESSAGES"],
-            throttling: {
-                usages: Globalcooldown.usage,
-                duration: Globalcooldown.duration
-            },
+            throttling: Globalcooldown.default,
         })
     }
     async run(message) {
-        let embed = new Discord.MessageEmbed()
-            .setAuthor(`Elara Support`, "https://cdn.discordapp.com/icons/499409162661396481/28c6fa39e722e2c0aea60f15ca105c1d.png?size=2048")
-            .setColor(message.guild ? message.guild.color : this.client.util.colors.default)
-            .setDescription(this.client.options.invite)
-        message.say(embed)
+        message.channel.send({embed: {
+            author: {
+                name: this.client.user.tag,
+                icon_url: this.client.user.displayAvatarURL()        
+            },
+            color: this.client.getColor(message.guild),
+            description: this.client.options.invite || "There isn't an invite set, contact the bot developer(s)"
+        }}).catch(() => {});
     }
 }
