f55c57bdf3e5970a6052d232b6751851a71eb59e
Elara Services <43923163+Elara-Discord-Bots@users.noreply.github.com>
2021-05-16 14:15:07 -0700

    Update help.js

--- src/commands/util/help.js
+++ src/commands/util/help.js
@@ -78,6 +78,7 @@ module.exports = class HelpCommand extends Command {
             .setColor(color)
             let display = new RichDisplay(e)
             groups.forEach(g => {
+		if(g.commands.size === 0) return;
                 let commands = g.commands.map(c => `**${c.name}**${c.nsfw ? "(NSFW)" : ""} - ${c.description}`)
                 if(g.id === "admin" && !message.member.hasPermission(`MANAGE_GUILD`)) return;
                 if(g.id === "mod" && !message.member.hasPermission("MANAGE_MESSAGES")) return;
